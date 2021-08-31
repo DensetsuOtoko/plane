249c77a156010202deec46b4850defc9f7edf7fa
Libio Junio <libiojss@gmail.com>
2021-08-31 10:52:23 -0300

    Setando componentes por código.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D meuRb;
+
+    void Start()
+    {
+        meuRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("pulou");
+        }
+    }
+}
